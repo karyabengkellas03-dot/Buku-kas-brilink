9cc8320b2ac74ff1b37d69107aa61a188d709d2d
karyabengkellas03-dot <karyabengkellas03@gmail.com>
2026-05-15 02:20:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ export default function App() {
   return (
     <div
       style={{
-        background: "black",
-        color: "white",
+        background: "#081028",
         minHeight: "100vh",
+        color: "white",
         padding: 20,
+        fontFamily: "sans-serif",
       }}
     >
       <h1>BRILink BukuKas</h1>
 
-      <h2>Aplikasi Berhasil</h2>
+      <h2>Aplikasi Normal</h2>
+
+      <div
+        style={{
+          background: "#1e293b",
+          padding: 20,
+          borderRadius: 20,
+          marginTop: 20,
+        }}
+      >
+        <h3>Saldo BRI</h3>
+
+        <h1>Rp 5.000.000</h1>
+      </div>
     </div>
   );
 }
