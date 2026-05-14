450285e6cad03ccc66069b8e06b6e347d4835fa5
karyabengkellas03-dot <karyabengkellas03@gmail.com>
2026-05-15 02:54:30 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ export default function App() {
       style={{
         background: "#081028",
         minHeight: "100vh",
-        color: "white",
         padding: 20,
+        color: "white",
         fontFamily: "sans-serif",
       }}
     >
       <h1>BRILink BukuKas</h1>
-
-      <h2>Aplikasi Normal</h2>
 
       <div
         style={{
@@ -21,9 +19,35 @@ export default function App() {
           marginTop: 20,
         }}
       >
-        <h3>Saldo BRI</h3>
+        <h2>Total Saldo</h2>
+        <h1>Rp 8.500.000</h1>
+      </div>
 
-        <h1>Rp 5.000.000</h1>
+      <div
+        style={{
+          background: "#1e293b",
+          padding: 20,
+          borderRadius: 20,
+          marginTop: 20,
+        }}
+      >
+        <h2>Menu</h2>
+
+        <button style={{ width: "100%", padding: 15, marginTop: 10 }}>
+          Input Transaksi
+        </button>
+
+        <button style={{ width: "100%", padding: 15, marginTop: 10 }}>
+          Saldo Akun
+        </button>
+
+        <button style={{ width: "100%", padding: 15, marginTop: 10 }}>
+          Rekap Harian
+        </button>
+
+        <button style={{ width: "100%", padding: 15, marginTop: 10 }}>
+          Rekap Bulanan
+        </button>
       </div>
     </div>
   );
