230409eb46f0164de873d438a5525d1f6320a51d
karyabengkellas03-dot <karyabengkellas03@gmail.com>
2026-05-14 20:32:56 +0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RafasyaCellApp from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RafasyaCellApp />
+  </React.StrictMode>
+);
