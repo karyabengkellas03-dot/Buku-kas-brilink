4d071676f2ea0a4eecc534df92b3f666676f18a2
karyabengkellas03-dot <karyabengkellas03@gmail.com>
2026-05-14 21:21:03 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RafasyaCellApp from "./App.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode> <RafasyaCellApp />
+<React.StrictMode> <App />
 </React.StrictMode>
 );
