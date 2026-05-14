05f18bc5cf0307a347c6574f8f1f826ceeeb53ff
karyabengkellas03-dot <karyabengkellas03@gmail.com>
2026-05-14 19:51:29 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+export const transaksi = [
+  {
+    tanggal: '16/05/2024',
+    jenis: 'SETOR TUNAI',
+    nominal: 5000000
+  },
+  {
+    tanggal: '16/05/2024',
+    jenis: 'TRANSFER',
+    nominal: 2500000
+  }
+]
